ff51a90a89bb5d2410f604eba2d86fda227a3be1
dave <david.petscher@gmail.com>
2020-10-18 19:07:57 +0200

    artifact

---
+++ build/precache-manifest.62ae96fabf8c511b137dbc303a462ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950337ce29268c9289bbc7e7bb19c0cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72ef95ab450ef8b0e682",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "299f71357a1e976073f4",
+    "url": "/static/js/2.b6e02204.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b6e02204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ef95ab450ef8b0e682",
+    "url": "/static/js/main.c16655b7.chunk.js"
+  },
+  {
+    "revision": "a72068537c3af4fdf6e5",
+    "url": "/static/js/runtime-main.1f43e603.js"
+  }
+]);
